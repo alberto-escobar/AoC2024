c0325542d0c6eb25bf69b6d77e84dca485ca76c7
aescobar96 <60338723+aescobar96@users.noreply.github.com>
2024-12-22 22:16:01 -0800

    day 21 complete

--- day21/main.py
+++ day21/main.py
@@ -1,3 +1,5 @@
+import re
+
 file = open("day21/data.txt")
 data = [row_string for row_string in file.read().split("\n")]
 
@@ -13,7 +15,19 @@ dpad = [
     ['<', 'v', '>']
 ]
 
-def shortest_paths_between_pads(keypad, start_position, end_pad):
+keypad_map = {
+            "7": (0, 0), "8": (1, 0), "9": (2, 0), 
+            "4": (0, 1), "5": (1, 1), "6": (2, 1),
+            "1": (0, 2), "2": (1, 2), "3": (2, 2), 
+            None: (0, 3), "0": (1, 3), "A": (2, 3)
+         }
+
+dpad_map = {
+            None: (0, 0), "^": (1, 0), "A": (2, 0), 
+            "<": (0, 1), "v": (1, 1), ">": (2, 1)
+        }
+
+def bfs(keypad, start_position, end_pad):
     queue=[(start_position, [], [])]
     min_paths = []
     min_steps = float("inf")
@@ -36,98 +50,88 @@ def shortest_paths_between_pads(keypad, start_position, end_pad):
 
             continue
         visited = visited + [curr_pad]
-        queue.append(((x, y+1),path+['v'], visited))
-        queue.append(((x-1, y),path+['<'], visited))
         queue.append(((x, y-1),path+['^'], visited))
         queue.append(((x+1, y),path+['>'], visited))
+        queue.append(((x, y+1),path+['v'], visited))
+        queue.append(((x-1, y),path+['<'], visited))
        
     return min_paths
 
-def find_shortest_sequences(keypad, string):
-    pad_map = {}
-    for j in range(len(keypad)):
-        for i in range(len(keypad[j])):
-            pad = keypad[j][i]
-            pad_map[pad] = (i,j)
-    paths = None
-    prev = "A"
-    for c in string:
-        current = pad_map[prev]
-        if not paths:
-            paths = shortest_paths_between_pads(keypad, current, c)
-        else:
-            new_paths = shortest_paths_between_pads(keypad, current, c)
-            temp = []
-            for path in paths:
-                for new_path in new_paths:
-                    temp.append(path+new_path)
-            paths = temp
-        prev = c
-    return paths
+shortest_paths = {}
 
-def compute_sequence_score(sequence):
-    prev = None
-    score = 0
-    for c in sequence:
-        if prev == None:
-            prev = c
-            continue
-        if c == prev:
-            score += 1
-    return score
-
-def find_shortest_sequence_length_improved(input, robots):
-    button_sequences = find_shortest_sequences(keypad, input)    
-    for robot in range(robots-1):
-        new_button_sequences = []
-        min = float("inf")
-        for input in button_sequences:
-            sequences = find_shortest_sequences(dpad, input)
-            if len(sequences[0]) > min:
+for start in ['7', '8', '9', '4', '5', '6', '1', '2', '3', '0', 'A']:
+    for row in keypad:
+        for pad in row:
+            if pad == None:
                 continue
-            if len(sequences[0]) < min:
-                min = len(sequences[0])
-                new_button_sequences = sequences
-            elif len(sequences[0]) == min:
-                new_button_sequences += sequences
-        
-        button_sequences = new_button_sequences
-        
-        best_sequences = []
-        best_score = -1
-        for sequence in button_sequences:
-            score = compute_sequence_score(sequence)
-            if score > best_score:
-                best_sequences = [sequence]
-                best_score = score
-            elif score == best_score:
-                best_sequences.append(sequence)
-        sequences = [best_sequences[0]]
-        
-    return len(sequences[0])
+            shortest_paths[(start, pad)] = bfs(keypad, keypad_map[start], pad)
 
-complexity_sum = 0
-for s in data:
-    length = find_shortest_sequence_length_improved(s, 3)
-    numeric_part = int(s[:-1])
-    complexity_sum += length * numeric_part
+for start in ['^', 'A', '<', 'v', '>']:
+    for row in dpad:
+        for pad in row:
+            if pad == None:
+                continue
+            shortest_paths[(start, pad)] = bfs(dpad, dpad_map[start], pad)
 
-print(f"part 1: {complexity_sum}")
+def build_sequence(keys, index, previous_key, current_sequence, result):
+    if index == len(keys):
+        result.append(current_sequence)
+        return
+    paths = shortest_paths[(previous_key, keys[index])]
+    for path in paths:
+        build_sequence(keys, index+1, keys[index], current_sequence + path, result)
 
-# When I increase the layers of robots from 3 to 4, I proceed to grind to a halt in my computations.
-# Seem like this problem as han exponentially increase amount of sequences at each layer, of the
-# robots.
+def find_shortest_sequence(keys, depth, cache):
+    if depth == 0:
+        return len(keys)
+    if (keys, depth) in cache:
+        return cache[(keys, depth)]
+    subKeys = re.findall('[<^>v]*A', keys)
+    total = 0
+    for subKey in subKeys:
+        result = []
+        build_sequence(subKey, 0, 'A', "", result)
+        min = float("inf")
+        for seq in result:
+            length = find_shortest_sequence(seq, depth-1, cache)
+            if length < min:
+                min = length
+        total += min
+    cache[(keys, depth)] = total
+    return total
 
-# below I will look to improve on my solution to solve part 2.
+cache = {}
+sum = 0
+for input in data:
+    result = []
+    build_sequence(input, 0, "A", "", result)
+    min = float('inf')
+    for seq in result:
+        length = find_shortest_sequence(seq, 2, cache)
+        if length < min:
+            min = length
+    numeric_part = int(input[:-1])
+    sum += min*numeric_part
 
-keypad = {
-            "7": (0, 0), "8": (0, 1), "9": (0, 2), 
-            "4": (1, 0), "5": (1, 1), "6": (1, 2),
-            "1": (2, 0), "2": (2, 1), "3": (2, 2), 
-            None: (3, 0), "0": (3, 1), "A": (3, 2)
-        }
+print(f"part 1: {sum}")
 
-dpad = {
-            None: (0, 0), "^": (0, 1), "A": (0, 2), 
-            "<": (1, 0), "v": (1, 1), ">": (1, 2)
-        }
+cache = {}
+sum = 0
+for input in data:
+    result = []
+    build_sequence(input, 0, "A", "", result)
+    min = float('inf')
+    for seq in result:
+        length = find_shortest_sequence(seq, 25, cache)
+        if length < min:
+            min = length
+    numeric_part = int(input[:-1])
+    sum += min*numeric_part
+
+print(f"part 2: {sum}")
+
+# this was a very difficult problem.
+# kudos to this reddit post, I was 
+# very lost and the post helped 
+# break it down to me
+# https://tinyurl.com/edbzn8w2
